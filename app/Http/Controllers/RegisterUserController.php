a633acabcbcf284621417386952c134240dec7aa
David Martinez <davidmgilo@gmail.com>
2017-02-21 21:59:27 +0100

    sync

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Events\NewRegisteredUserEvent;
+use App\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Log;
@@ -11,12 +12,13 @@ class RegisterUserController extends Controller
 {
     public function register()
     {
-        $user = new \App\User();
-        $user->name = 'Pepito Palotes';
-        $user->email = 'sergiturbadenas@gmail.com';
+        $user = User::find(2);
+//        $user = new \App\User();
+//        $user->name = 'Pepito Palotes';
+//        $user->email = 'sergiturbadenas@gmail.com';
         Log::info('Before event');
         event(new Registered($user));
         Log::info('After event');
-        dump("done!");
+        return view('mails.welcome');
     }
 }
--- app/Listeners/WelcomeEmailListener.php
+++ app/Listeners/WelcomeEmailListener.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Mail;
 
-class WelcomeEmailListener
+class WelcomeEmailListener implements ShouldQueue
 {
     /**
      * Create the event listener.
